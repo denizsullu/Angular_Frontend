ba89b87209308d35c8b8364dc400713dcf09a57c
deniz <denizsullu@protonmail.com>
2024-01-08 01:13:37 +0300

    Use httpclient and  Built-in control flow Angular 17

--- 5-Angular/Personel Dev/northwind/src/app/components/product/product.component.ts
+++ 5-Angular/Personel Dev/northwind/src/app/components/product/product.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
+import {Product} from "../../models/product";
+import {Data} from "../../models/data";
+import {HttpClient} from "@angular/common/http";
+import {ProductResponseModel} from "../../models/productResponseModel";
 
 @Component({
   selector: 'app-product',
@@ -10,13 +14,22 @@ import {NgForOf} from "@angular/common";
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
-  product1: any = {productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5}
-  product2: any = {productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5}
-  product3: any = {productId: 3, productName: 'Mous', categoryId: 1, unitPrice: 5}
-  product4: any = {productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5}
-  product5: any = {productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5}
-  products: any = [this.product1, this.product2, this.product3, this.product4, this.product5]
-constructor() {}
+export class ProductComponent implements OnInit{
+  item: string = "deniz";
+  apiUrl: string ="http://localhost:3000/data";
+  // productResponseModel: ProductResponseModel;
+  products: Product[] = []
+  data: Data[] = [];
+constructor(private htttpClient:HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts(){
+    this.htttpClient.get<Data[]>(this.apiUrl)
+      .subscribe((response) => {
+      this.data = response;
+    });
+  }
 
 }
---
+++ 5-Angular/Personel Dev/northwind/src/app/models/data.ts
@@ -0,0 +1,6 @@
+export interface Data {
+  productId: number;
+  categoryId: number;
+  productName: string;
+  unitPrice: number;
+}
